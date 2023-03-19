688ffc37e8ee703b0e2cb66624e61a6756369f1a
Meng-Xin <li997266754@gmail.com>
2023-03-19 21:22:08 +0800

    fix：调整http代理链接最大空闲时间 为 168小时(一周)

--- utils/agent.go
+++ utils/agent.go
@@ -4,6 +4,7 @@ import (
 	openai "github.com/sashabaranov/go-gpt3"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func InitOpenAiAgent(token string, proxyPath string) openai.ClientConfig {
@@ -13,7 +14,8 @@ func InitOpenAiAgent(token string, proxyPath string) openai.ClientConfig {
 		panic(err)
 	}
 	transport := &http.Transport{
-		Proxy: http.ProxyURL(proxyUrl),
+		Proxy:           http.ProxyURL(proxyUrl),
+		IdleConnTimeout: 168 * time.Duration(time.Hour),
 	}
 	config.HTTPClient = &http.Client{
 		Transport: transport,
